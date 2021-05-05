518e31c01298cd8aca442dedfa563c72bcb47bf5
Binh Hoang <Binh.Hoang@kiwiwealth.co.nz>
2021-05-05 18:14:59 +1200

    need to go now - later for pictures

---
+++ pictures.js
@@ -0,0 +1,11 @@
+var picture_list =
+[
+    "https://vhpainting.co.nz/wp-content/uploads/2019/12/Exterior-Before.jpg",
+    "https://vhpainting.co.nz/wp-content/uploads/2019/12/Exterior-During.jpg",
+    "https://vhpainting.co.nz/wp-content/uploads/2019/12/Interior-Painting-and-Varnishing.jpg",
+    "https://vhpainting.co.nz/wp-content/uploads/2019/12/Interior-Windows-Plaster.jpg",
+    "https://vhpainting.co.nz/wp-content/uploads/2019/12/Interior-Windows-Plastering.jpg",
+    "https://vhpainting.co.nz/wp-content/uploads/2019/12/Interior-Windows-After.jpg",
+    "https://vhpainting.co.nz/wp-content/uploads/2019/12/Interior-Windows-Finished.jpg"
+]; 
+

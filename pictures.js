8caf08af405f9dd9d4892ea6adcee2b738d53338
Binh Hoang <Binh.Hoang@kiwiwealth.co.nz>
2021-05-07 11:55:19 +1200

    phan nguoi xac nhan da xong - kiem tra voi moi nguoi

--- pictures.js
+++ pictures.js
@@ -9,3 +9,13 @@ var picture_list =
     "https://vhpainting.co.nz/wp-content/uploads/2019/12/Interior-Windows-Finished.jpg"
 ]; 
 
+document.getElementById("pictures").querySelector(".carousel-inner").innerHTML = picture_list.reduce 
+(
+    (accumulator, current_value, index)=>
+    accumulator + 
+    `
+        <div class="carousel-item ${index==0?"active": ""}">
+            <img src="${current_value}" alt="${current_value}" />
+        </div>
+    `, ""
+); 

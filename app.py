8babdcce35f87cf7e1e1b70e03eebd99228a6c51
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-05-09 16:35:58 +1200

    cuoi cung cung chay roi -

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html")
+@app.route("/<path:path>")
+def js_path(path): 
+    print(path)
+    return send_from_directory("templates", path)
+
+@app.route("/send-email", methods=["POST"])
+def send_email(): 
+    print(request)
+    print(request.form)
+    print(request.form['test'])
+    return "123"
+
 
 if __name__=="__main__": 
     app.run()
